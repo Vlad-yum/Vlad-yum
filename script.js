7943ae8302969ef235e6fc391231ad45bcdbe01c
Vlad <example@gmail.com>
2020-04-05 23:25:10 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+let person = {
+    name: "Leon",
+    age:25,
+    isMarriad: false
+};
+
+// console.log(person.isMarriad);
+
+//  let arr = ["pium.png","orange.jpg","apple.bmp"]; //masive
+
+// console.log(arr[2]);
+// //alert("hello");
+
+//  let answer  = prompt("hello?","yes");
+
+// console.log(typeof(arr));
+
+
+let incr= 10;
+
+let decr= 10;
+
+
+
+console.log(++incr);
+console.log(decr--);
+
+
+console.log(5%2);le
+let isChacked = true;
+let isClose= true;
+
+console.log(isChacked && isClose);
